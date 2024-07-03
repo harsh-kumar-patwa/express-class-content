8fe206dcb29047e7dbd08f92080b08e3804889fa
Harsh Kumar <harshkumar3446@gmail.com>
2024-07-03 10:13:38 +0530

    added post request

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let courses = [
     {id:1, name:'course1'},
     {id:2, name:'course2'},
@@ -10,5 +12,13 @@ let courses = [
 app.get('/courses',(req,res)=>{
     res.json(courses);
 });
-
+app.post('/courses',(req,res)=>{
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    console.log(req.body);
+    
+});
 app.listen(3000,()=>{console.log('Listening on port 3000...')});
