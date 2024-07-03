1b7f338f6f70ef912b0b3d7cea8297382f03fa0e
Harsh Kumar <harshkumar3446@gmail.com>
2024-07-03 10:36:14 +0530

    update and delete added

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ let courses = [
     {id:2, name:'course2'},
     {id:3, name:'course3'}
 ]
-
+//get call
 app.get('/courses',(req,res)=>{
     res.json(courses);
 });
+//post call
 app.post('/courses',(req,res)=>{
     const course = {
         id: courses.length + 1,
@@ -19,6 +20,31 @@ app.post('/courses',(req,res)=>{
     };
     courses.push(course);
     console.log(req.body);
-    
 });
+//update call
+app.put('/courses/:id',(req,res)=>{
+    const course = courses.find(
+        c=>c.id === parseInt(req.params.id)
+    );
+    if(!course) {
+        console.log('Course not found');
+        return;
+    };
+    course.name = req.body.name;
+    res.send(course);
+});
+//delete call
+app.delete('/courses/:id',(req,res)=>{
+    const course = courses.find(
+        c=>c.id === parseInt(req.params.id)
+    );
+    if(!course) {
+        console.log('Course not found');
+        return;
+    };
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+    res.send(courses);
+});
+//listen the port 
 app.listen(3000,()=>{console.log('Listening on port 3000...')});
