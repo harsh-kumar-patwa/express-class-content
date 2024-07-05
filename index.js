2f25399c747c18d3e585f4ee26f16000430b9975
Harsh Kumar <harshkumar3446@gmail.com>
2024-07-05 10:54:07 +0530

    added mongoose schema

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://HarshKumarPatwa:Harsh3446@cluster0.mccbu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    .then(() => {
+        console.log("Connection Successful");
+    })
+    .catch((err) => {
+        console.log("Failed",err);
+    });
+
+const productSchema = new mongoose.Schema({
+    product_name: {
+        type: String,
+        required: true
+    },
+    product_price: {
+        type: String,
+        required: true
+    },
+    isInStock:{
+        type:Boolean,
+        required:true
+    },
+    Category:{
+        type:String,
+        required:true
+    
+    }
+    
+});
 
 app.use(express.json());
 app.use(middleware);
 app.use(logger);
 let courses = [
-    {id:1, name:'course1'},
-    {id:2, name:'course2'},
-    {id:3, name:'course3'}
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
 ]
 //get call
-app.get('/courses',(req,res)=>{
+app.get('/courses', (req, res) => {
     res.json(courses);
 });
 //post call
-app.post('/courses',(req,res)=>{
+app.post('/courses', (req, res) => {
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -23,11 +52,11 @@ app.post('/courses',(req,res)=>{
     // console.log(req.body);
 });
 //update call
-app.put('/courses/:id',(req,res)=>{
+app.put('/courses/:id', (req, res) => {
     const course = courses.find(
-        c=>c.id === parseInt(req.params.id)
+        c => c.id === parseInt(req.params.id)
     );
-    if(!course) {
+    if (!course) {
         console.log('Course not found');
         return;
     };
@@ -35,21 +64,21 @@ app.put('/courses/:id',(req,res)=>{
     res.send(course);
 });
 //delete call
-app.delete('/courses/:id',(req,res)=>{
+app.delete('/courses/:id', (req, res) => {
     const course = courses.find(
-        c=>c.id === parseInt(req.params.id)
+        c => c.id === parseInt(req.params.id)
     );
-    if(!course) {
+    if (!course) {
         console.log('Course not found');
         return;
     };
     const index = courses.indexOf(course);
-    courses.splice(index,1);
+    courses.splice(index, 1);
     res.send(courses);
 });
 
 //custom middle ware
-function middleware(req,res,next){
+function middleware(req, res, next) {
     console.log('called...');
     next();
 }
@@ -61,4 +90,4 @@ function logger(req, res, next) {
 }
 
 //listen the port 
-app.listen(3000,()=>{console.log('Listening on port 3000...')});
+app.listen(3000, () => { console.log('Listening on port 3000...') });
